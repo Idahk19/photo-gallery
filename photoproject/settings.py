5973ba63457b3f75922745a1df820ff629735426
Idahk19 <idahkarwitha9@gmail.com>
2026-09-02 20:01:04 +0300

    Add .env file for secret key management and update settings.py to use environment variable

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6axv#-c5r@@wkd2fp9sm6=85(wcoe5(znun!b-5r25dfz@31v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
